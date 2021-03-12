795ef29ded0c1cdff317d73376d51be98d3c7e2e
Lautaro Jayat <lautaro.jayat@webeelife.com>
2021-03-12 18:10:26 -0300

    Added one example using channels

---
+++ wait_groups/using_channels/main.go
@@ -0,0 +1,41 @@
+package main
+
+import (
+	"fmt"
+	"sync"
+	"time"
+)
+
+func main(){
+
+	var w sync.WaitGroup
+
+	fmt.Println("Starting channels example")
+	fmt.Println("spawing all of you pals")
+	c:= make(chan string, 1)
+	for i:= 0; i < 10; i++{
+		w.Add(1)
+		go func(i int){
+			fmt.Println("channel", i, " just waiting");
+			<-c
+			fmt.Println("hi from channel", i, "now terminating");
+			w.Done()
+		}(i)
+	}
+
+	fmt.Println("\nIm going to sleep now");
+	fmt.Println("\n");
+
+	time.Sleep(time.Second * 5)
+	fmt.Println("\nHey Guys, here from main function ,how are you doing!");
+	fmt.Println("\n");
+
+	for j:=0; j<10; j++{
+		c <- "Hi to every rutine, please respond"
+	}
+	w.Wait()
+
+	fmt.Println("\nNice to have news from you guys");
+
+
+}
